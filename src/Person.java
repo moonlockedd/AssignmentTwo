076b0cd62ad0c5dd7bf7017582939776574e60c5
Nurali <231087@astanait.edu.kz>
2024-01-19 10:20:41 +0600

    Add getters, setters, and toString to Person class

--- src/Person.java
+++ src/Person.java
@@ -14,5 +14,29 @@ public class Person {
         this.surname = surname;
     }
 
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " +
+                name + " " +
+                surname;
+    }
 }
