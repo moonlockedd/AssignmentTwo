e8c0dd560e176df0de740f8a4596d7277bc52e34
Nurali <231087@astanait.edu.kz>
2024-01-19 10:33:17 +0600

    Create Employee class

---
+++ src/Employee.java
@@ -0,0 +1,36 @@
+public class Employee extends Person {
+    private String position;
+    private double salary;
+
+    Employee() {
+        super();
+    }
+
+    Employee(String name, String surname, String position, double salary) {
+        super(name, surname);
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return position + ": " +
+                super.toString();
+    }
+}
