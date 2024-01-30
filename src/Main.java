7350c083507084a0ece6f4b4470348a5308a7658
Nurali <231087@astanait.edu.kz>
2024-01-30 16:03:24 +0600

    Add Student, Employee objects directly to an ArrayList

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,28 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        // Create some students and employees
-        Student student1 = new Student(
-                "Brian", "May", 3.66);
-        Student student2 = new Student(
-                "Roger", "Taylor", 3.1);
-        Student student3 = new Student(
-                "Freddie", "Mercury", 2.49);
-        Employee employee1 = new Employee(
-                "Elvis", "Presley", "Singer", 173333.33);
-        Employee employee2 = new Employee(
-                "John", "Deacon", "Bass-player", 120499.99);
-        Employee employee3 = new Employee(
-                "Elton", "John", "Singer", 250000);
-
         // Create and instantiate an arraylist
         ArrayList<Person> people = new ArrayList<>();
 
-        // Add students and employees to arraylist
-        people.add(student1);
-        people.add(student2);
-        people.add(student3);
-        people.add(employee1);
-        people.add(employee2);
-        people.add(employee3);
+        // Add Student and Employee instances to ArrayList
+        people.add(new Student("Brian", "May", 3.66));
+        people.add(new Student("Roger", "Taylor", 3.1));
+        people.add(new Student("Freddie", "Mercury", 2.49));
+        people.add(new Employee("Elvis",
+                "Presley",
+                "Singer",
+                173333.33
+        ));
+        people.add(new Employee("Elton",
+                "John",
+                "Singer",
+                250000
+        ));
+        people.add(new Employee("John",
+                "Deacon",
+                "Bass-player",
+                120499.99
+        ));
 
         // Sort by their pay amount in ascending order
         Collections.sort(people);
